0611b8fbfda2af99f31ec0a08511c7fe65db3b7d
nghiatt <nghiatt@coccoc.com>
2019-04-01 10:49:56 +0700

    windows: Remove switching virtual desktops script

--- windows/misc.ahk
+++
@@ -1,2 +0,0 @@
-#1:: sendevent {LWin down}{LCtrl down}{Left down}{LWin up}{LCtrl up}{Left up}    ; switch to previous virtual desktop
-#2:: sendevent {LWin down}{LCtrl down}{Right down}{Lwin up}{LCtrl up}{Right up}      ; switch to next virtual desktop
--- windows/windows.py
+++ windows/windows.py
@@ -15,5 +15,4 @@ def setup():
     current_folder = os.path.dirname(os.path.realpath(__file__))
     run_command("schtasks /CREATE /SC ONLOGON /TN Launch-Apps /TR {} /RL LIMITED".format(os.path.join(current_folder, "launch_apps.ahk")))
     run_command("schtasks /CREATE /SC ONLOGON /TN xlaunch /TR {} /RL LIMITED".format(os.path.join(current_folder, "config.xlaunch")))
-    run_command("schtasks /CREATE /SC ONLOGON /TN Misc /TR {} /RL HIGHEST".format(os.path.join(current_folder, "misc.ahk")))
     run_command('copy %HOMEPATH%\\myconfig\\windows\\urxvt.lnk "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs"')
