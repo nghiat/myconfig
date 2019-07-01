440ad731da1e9175467d854179634e6ac1a4e9e4
nghiatt <nghiatt@coccoc.com>
2019-07-01 16:58:22 +0700

    windows: Add QTTabBarConfig and some explorer settings

--- windows/windows.py
+++ windows/windows.py
@@ -4,7 +4,11 @@ from scripts.subprocess_wrapper import run_command
 note = """clink
 autohotkey
 vs-ez
-vcxsrv"""
+vcxsrv
+QTTabBar
+Preview pane
+Folder Options: Quick access
+"""
 
 colors_templates = {
     ".minttyrc.template": "~/.minttyrc",
@@ -16,3 +20,9 @@ def setup():
     run_command("schtasks /CREATE /SC ONLOGON /TN Launch-Apps /TR {} /RL LIMITED".format(os.path.join(current_folder, "launch_apps.ahk")))
     run_command("schtasks /CREATE /SC ONLOGON /TN xlaunch /TR {} /RL LIMITED".format(os.path.join(current_folder, "config.xlaunch")))
     run_command('copy %HOMEPATH%\\myconfig\\windows\\urxvt.lnk "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs"')
+    run_command('reg IMPORT edit_with_vim.reg')
+    run_command('reg IMPORT preview.reg')
+    run_command('taskkill /f /im explorer.exe')
+    run_command('start explorer.exe')
+
+
