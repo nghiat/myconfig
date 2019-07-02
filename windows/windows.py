997468b4c14b63a0ae4ed5d31e37071d4a09e2b5
nghiatt <nghiatt@coccoc.com>
2019-07-02 10:20:28 +0700

    windows: stop running edit_with_vim.reg automatically

--- windows/windows.py
+++ windows/windows.py
@@ -8,6 +8,7 @@ vcxsrv
 QTTabBar
 Preview pane
 Folder Options: Quick access
+Edit and run: edit_with_vim.reg
 """
 
 colors_templates = {
@@ -20,7 +21,6 @@ def setup():
     run_command("schtasks /CREATE /SC ONLOGON /TN Launch-Apps /TR {} /RL LIMITED".format(os.path.join(current_folder, "launch_apps.ahk")))
     run_command("schtasks /CREATE /SC ONLOGON /TN xlaunch /TR {} /RL LIMITED".format(os.path.join(current_folder, "config.xlaunch")))
     run_command('copy %HOMEPATH%\\myconfig\\windows\\urxvt.lnk "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs"')
-    run_command('reg IMPORT edit_with_vim.reg')
     run_command('reg IMPORT preview.reg')
     run_command('taskkill /f /im explorer.exe')
     run_command('start explorer.exe')
