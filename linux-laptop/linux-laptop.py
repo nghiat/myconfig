f4a4758e7ad836cb6556cb8194dcb32fb71a512e
nghiat <trantuannghia95@gmail.com>
2020-01-03 02:32:54 +0700

    linux-laptop: disable beep by default

--- linux-laptop/linux-laptop.py
+++ linux-laptop/linux-laptop.py
@@ -3,14 +3,14 @@ from scripts.subprocess_wrapper import run_script
 links = {
     "/etc/X11/xorg.conf.d/30-touchpad.conf": "30-touchpad.conf",
     "/etc/acpi/events/lid": "lid",
-    "/etc/acpi/actions/lid.sh": "lid.sh"
+    "/etc/acpi/actions/lid.sh": "lid.sh",
 }
 
 note = """acpid, tlp
-# disable pc speaker
-echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+nvidia-prime
 """
 
 
 def setup():
     run_script("laptop.sh")
+    run_command("echo blacklist pcspkr | sudo tee /etc/modprobe.d/nobeep.conf > /dev/null")
