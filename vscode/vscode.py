503007f42f1fdbd65d32a6bcb96105c8fcd2476e
nghiat <trantuannghia95@gmail.com>
2020-10-03 20:58:00 +0700

    vscode: add ez theme

--- vscode/vscode.py
+++ vscode/vscode.py
@@ -4,6 +4,15 @@ from scripts.subprocess_wrapper import run_command
 
 note = """code.cmd on Windows"""
 
+colors_templates = {
+    "./ez-theme/themes/ez-theme.json.template": "./ez-theme/themes/ez-theme.json",
+    "./ez-theme/themes/ez-dark-theme.json.template": "./ez-theme/themes/ez-dark-theme.json",
+}
+
+links = {
+    "~/.vscode/extensions/ez-theme": "ez-theme",
+}
+
 extension_ids = [
     "christian-kohler.path-intellisense",
     "donjayamanne.githistory",
@@ -14,7 +23,6 @@ extension_ids = [
     "robertohuertasm.vscode-icons",
     "vscodevim.vim",
     "xaver.clang-format",
-    "cquery-project.cquery",
 ]
 
 files = ["keybindings.json", "settings.json"]
@@ -23,18 +31,17 @@ files = ["keybindings.json", "settings.json"]
 def create_links(prefix_path):
     results = {}
     for filename in files:
-        results[os.path.join(prefix_path, filename)] = filename
-    return results
+        links[os.path.join(prefix_path, filename)] = filename
 
 code_cmd = "code"
 
 if platform.system() == "Windows":
-    links = create_links("~/AppData/Roaming/Code/User")
+    create_links("~/AppData/Roaming/Code/User")
     code_cmd = "code.cmd"
 elif platform.system() == "Linux":
-    links = create_links("~/.config/Code/User")
+    create_links("~/.config/Code/User")
 elif platform.system() == "Darwin":
-    links = create_links("~/Library/Application Support/Code/User")
+    create_links("~/Library/Application Support/Code/User")
 
 
 def setup():
