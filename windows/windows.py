3fdb009ac70a3debd0c0811330daef8485d2b392
nghiat <trantuannghia95@gmail.com>
2021-11-16 00:10:09 +0100

    windows: Fix some commands not working with space in path

--- windows/windows.py
+++ windows/windows.py
@@ -37,15 +37,15 @@ links = {
 
 def setup():
     current_folder = os.path.dirname(os.path.realpath(__file__))
-    run_command("schtasks /CREATE /SC ONLOGON /TN Launch-Apps /TR {} /RL LIMITED".format(os.path.join(current_folder, "launch_apps.ahk")))
-    run_command("schtasks /CREATE /SC ONLOGON /TN xlaunch /TR {} /RL LIMITED".format(os.path.join(current_folder, "config.xlaunch")))
-    run_command('copy %HOMEPATH%\\myconfig\\windows\\urxvt.lnk "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs"')
+    run_command('schtasks /CREATE /SC ONLOGON /TN Launch-Apps /TR "AutoHotKey.exe \'{}\'" /RL LIMITED'.format(os.path.join(current_folder, "launch_apps.ahk"))) # Exe path is case-sensitive
+    run_command('schtasks /CREATE /SC ONLOGON /TN xlaunch /TR "xlaunch.exe -run \'{}\'" /RL LIMITED'.format(os.path.join(current_folder, "config.xlaunch"))) # Exe path is case-sensitive
+    run_command('copy "%HOMEPATH%\\myconfig\\windows\\urxvt.lnk" "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs"')
     run_command('reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_QWORD /f')
     # Windows startup delay
     run_command('reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Serialize" /v StartupDelayInMSec /d 0 /t REG_DWORD /f')
     run_command('reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v BingSearchEnabled /d 0 /t REG_DWORD /f')
     run_command('reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" /v CortanaConsent /d 0 /t REG_DWORD /f')
     # Free Commander
-    run_command('mkdir %HOMEPATH%\\AppData\\Local\\FreeCommanderXE\\Settings')
-    run_command('copy %HOMEPATH%\\myconfig\\windows\\FreeCommander.ini "%HOMEPATH%\\AppData\\Local\\FreeCommanderXE\\Settings"')
-    run_command('copy %HOMEPATH%\\myconfig\\windows\\FreeCommander.shc "%HOMEPATH%\\AppData\\Local\\FreeCommanderXE\\Settings"')
+    run_command('mkdir "%HOMEPATH%\\AppData\\Local\\FreeCommanderXE\\Settings"')
+    run_command('copy "%HOMEPATH%\\myconfig\\windows\\FreeCommander.ini" "%HOMEPATH%\\AppData\\Local\\FreeCommanderXE\\Settings"')
+    run_command('copy "%HOMEPATH%\\myconfig\\windows\\FreeCommander.shc" "%HOMEPATH%\\AppData\\Local\\FreeCommanderXE\\Settings"')
