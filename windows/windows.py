0b934d3b42a94fdbbd294c33ea61c0a19a9dd87a
nghiat <trantuannghia95@gmail.com>
2023-07-01 13:28:13 +0200

    windows: automate old context menu, task scheduler settings

--- windows/windows.py
+++ windows/windows.py
@@ -1,7 +1,9 @@
 import os
 from scripts.subprocess_wrapper import run_command
 
-note = """clink (github)
+note = r"""
+Envvars: C:\Program Files\Google\Chrome\Application;C:\Program Files\Everything;D:\apps\vim;
+clink (github)
 vcxsrv
 Put perl bin dir before git/bin in PATH because fzf :Tags doesn't work well with perl from git
 https://tuxproject.de/projects/vim/
@@ -24,8 +26,6 @@ HiDPI:
 
 Windows 10: Settings -> Accounts -> Sign-in options -> Disble "Use my sign-in info ..."
 clink set clink.default_bindings bash
-
-https://github.com/JacobMS2020/Old-Windows-10-context-menu
 """
 
 colors_templates = {
@@ -38,8 +38,8 @@ links = {
 
 def setup():
     current_folder = os.path.dirname(os.path.realpath(__file__))
-    run_command('schtasks /CREATE /SC ONLOGON /TN Launch-Apps /TR "AutoHotKey.exe \'{}\'" /RL LIMITED'.format(os.path.join(current_folder, "launch_apps.ahk"))) # Exe path is case-sensitive
-    run_command('schtasks /CREATE /SC ONLOGON /TN xlaunch /TR "xlaunch.exe -run \'{}\'" /RL LIMITED'.format(os.path.join(current_folder, "config.xlaunch"))) # Exe path is case-sensitive
+    run_command('schtasks /CREATE /TN Launch-Apps /xml "{}"'.format(os.path.join(current_folder, "Launch-Apps.xml")))
+    run_command('schtasks /CREATE /TN xlaunch /xml "{}"'.format(os.path.join(current_folder, "xlaunch.xml")))
     run_command('copy "%HOMEPATH%\\myconfig\\windows\\urxvt.lnk" "%AppData%\\Microsoft\\Windows\\Start Menu\\Programs"')
     run_command('reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\TimeZoneInformation" /v RealTimeIsUniversal /d 1 /t REG_QWORD /f')
     # Windows startup delay
@@ -52,3 +52,5 @@ def setup():
     run_command('copy "%HOMEPATH%\\myconfig\\windows\\FreeCommander.shc" "%HOMEPATH%\\AppData\\Local\\FreeCommanderXE\\Settings"')
     # choco
     run_command('powershell -ExecutionPolicy Bypass -File {}'.format(os.path.join(current_folder, "chocolatey.ps1")))
+    # Windows 10 old context
+    run_command('reg add "HKEY_CURRENT_USER\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /t REG_SZ /f')
