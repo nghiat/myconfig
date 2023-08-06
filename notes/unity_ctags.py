39cdbc89e51ab33b616ac0e3c67e0bdbcfff2597
nghiat <trantuannghia95@gmail.com>
2023-08-06 21:56:05 +0200

    notes: Parallelize unity ctags script

--- notes/unity_ctags.py
+++ notes/unity_ctags.py
@@ -1,9 +1,24 @@
+from pathlib import Path
+
+import concurrent.futures
 import glob
 import os
-from pathlib import Path
 import shutil
 import subprocess
 import sys
+import threading
+
+
+print_lock = threading.Lock()
+
+
+def decompile(cmd):
+    with print_lock:
+        print(f'Running: "{cmd}"')
+    p = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    with print_lock:
+        print(f'"{cmd}" output: ' + p.stdout)
+
 
 def main():
     if shutil.which("ilspycmd") == None:
@@ -19,12 +34,14 @@ def main():
     in_dir = sys.argv[1]
     out_dir = sys.argv[2]
     dll_files = glob.glob(os.path.join(in_dir, "*.dll"))
+    executor = concurrent.futures.ProcessPoolExecutor(max_workers=4)
+    futures = []
     for dll in dll_files:
         output_subdir = Path(dll).stem
         output_fulldir = os.path.join(out_dir, output_subdir)
         cmd = f"ilspycmd -o {output_fulldir} -p {dll}"
-        print("Running: " + cmd)
-        p = subprocess.run(cmd, shell=True)
+        futures.append(executor.submit(decompile, cmd))
+    concurrent.futures.as_completed(futures)
 
 
 if __name__ ==  "__main__":
